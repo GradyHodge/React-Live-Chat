95c164a58ca27f461bf3e112af522ea6a20e77d3
Grady Hodge <gradywhodge@gmail.com>
2020-02-06 14:08:08 -0600

    set up 4 sections in dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,9 @@ import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -12,7 +15,8 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2)
   },
   flex: {
-    display: "flex"
+    display: "flex",
+    WebkitAlignItems: "center"
   },
   topicsWindow: {
     width: "30%",
@@ -21,7 +25,8 @@ const useStyles = makeStyles(theme => ({
   },
   chatWindow: {
     width: "70%",
-    height: "300px"
+    height: "300px",
+    padding: "20px"
   },
   chatBox: {
     width: "85%"
@@ -33,6 +38,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function Dashboard() {
   const classes = useStyles();
+  const [textValue, changeTextValue] = React.useState(""); // React Hook - initializing state set to nothing.
 
   return (
     <div>
@@ -53,10 +59,27 @@ export default function Dashboard() {
               ))}
             </List>
           </div>
-          <div className={classes.chatWindow}></div>
+          <div className={classes.chatWindow}>
+            {[{ from: "user", msg: "hello" }].map((chat, i) => (
+              <div className={classes.flex} key={i}>
+                <Chip label={chat.from} className={classes.chip} />
+                <Typography variant="p">{chat.msg}</Typography>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className={classes.flex}></div>
+        <div className={classes.flex}>
+          <TextField
+            label="Send a chat"
+            className={classes.chatBox}
+            value={textValue}
+            onChange={e => changeTextValue(e.target.value)}
+          />
+          <Button variant="contained" color="primary">
+            Send
+          </Button>
+        </div>
       </Paper>
     </div>
-  );
-}
+  ); //end Return
+} //end component
